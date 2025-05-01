94cd66d723473e270922f874c04e47c226ea9a51
Tiloyeade <adeabiodun2020@gmail.com>
2025-05-01 11:29:19 +0100

    updated

--- src/components/bookFreeTrial/SecureFuture.tsx
+++ src/components/bookFreeTrial/SecureFuture.tsx
@@ -6,6 +6,7 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 // Removed unused bgImage import as a hardcoded URL is used below
 
 
+
 const SecureFuture: React.FC = () => { // Define component type
   return (
     <Container maxWidth="xl" sx={{
